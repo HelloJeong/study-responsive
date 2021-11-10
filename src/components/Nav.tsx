80d1c8590716680aa11a95e89d8261b751e42c82
HelloJeong <jeongyk92@gmail.com>
2021-11-10 17:28:47 +0900

    Nav Component - PC화면 디자인

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,49 @@
+import REACT_LOGO from "../images/React_Logo.svg";
+import styled from "styled-components";
+
 const Nav: React.FC = () => {
-  return <div>NavBar</div>;
+  return (
+    <Common>
+      <Home href="/">React</Home>
+      <LogoImg src={REACT_LOGO} />
+
+      <ControlBox>
+        <Control href="/connect">계약</Control>
+        <Control href="/login">로그인</Control>
+        <Control href="/signup">회원가입</Control>
+      </ControlBox>
+    </Common>
+  );
 };
+
+const Common = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px;
+`;
+
+const Home = styled.a`
+  display: flex;
+  align-items: center;
+  font-size: 30px;
+  margin: 10px;
+  text-decoration: none;
+  color: black;
+  font-weight: bold;
+`;
+const LogoImg = styled.img`
+  width: 100px;
+`;
+
+const ControlBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Control = styled.a`
+  margin: 10px;
+  text-decoration: none;
+  color: black;
+`;
 
 export default Nav;
