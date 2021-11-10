226eaf7136ff4b194f011bce47d2b15d4fda3544
HelloJeong <jeongyk92@gmail.com>
2021-11-10 18:03:47 +0900

    NavBar - 모바일 대응, 메뉴 열기

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,25 +1,40 @@
 import REACT_LOGO from "../images/React_Logo.svg";
 import styled from "styled-components";
+import { IoIosMenu } from "react-icons/io";
+import { useState } from "react";
+import { IControlBoxProps } from "../type";
 
 const Nav: React.FC = () => {
+  const [menu, setMenu] = useState<boolean>(false);
+
   return (
     <Common>
       <Home href="/">React</Home>
       <LogoImg src={REACT_LOGO} />
 
-      <ControlBox>
+      <ControlBox menu={menu}>
         <Control href="/connect">계약</Control>
         <Control href="/login">로그인</Control>
         <Control href="/signup">회원가입</Control>
       </ControlBox>
+      <Menubar href="#" onClick={onClickMenu}>
+        <IoIosMenu />
+      </Menubar>
     </Common>
   );
+
+  function onClickMenu() {
+    setMenu(!menu);
+  }
 };
 
 const Common = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 20px;
+  @media screen and (max-width: 500px) {
+    flex-direction: column;
+  }
 `;
 
 const Home = styled.a`
@@ -30,20 +45,40 @@ const Home = styled.a`
   text-decoration: none;
   color: black;
   font-weight: bold;
+  @media screen and (max-width: 500px) {
+    display: none;
+  }
 `;
 const LogoImg = styled.img`
   width: 100px;
 `;
 
-const ControlBox = styled.div`
+const ControlBox = styled.div<IControlBoxProps>`
   display: flex;
   align-items: center;
+  @media screen and (max-width: 500px) {
+    flex-direction: column;
+    align-items: flex-end;
+    display: ${({ menu }) => (menu ? "flex" : "none")};
+  }
 `;
 
 const Control = styled.a`
   margin: 10px;
   text-decoration: none;
   color: black;
+`;
+
+const Menubar = styled.a`
+  display: flex;
+  align-items: center;
+  font-size: 30px;
+  position: absolute;
+  right: 32px;
+  height: 97px;
+  @media screen and (min-width: 500px) {
+    display: none;
+  }
 `;
 
 export default Nav;
---
+++ src/type.ts
@@ -0,0 +1,3 @@
+export interface IControlBoxProps {
+  menu: boolean;
+}
